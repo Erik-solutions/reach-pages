55eebfaa79d9170831793accc0de5de4da8efe9e
Erik-solutions <info@erik19-analytics.com>
2025-03-27 19:07:44 +0200

    all

--- smb/src/App.jsx
+++ smb/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar  from './components/navbar'
+import LandingPage from './components/landingPage'
 import './css/App.css'
 
 export default function App() {
  
 
   return (
-
+<>
     <NavBar/>
+    <LandingPage/>
+    </>
   );
 
 }
---
+++ smb/src/components/landingPage.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function LandingPage() {
+
+
+
+    return(
+<p>This is p on</p>
+
+    );
+}
+
