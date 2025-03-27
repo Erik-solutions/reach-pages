dae45c7567fdb892dc501b8763098b26debd514b
Erik-solutions <info@erik19-analytics.com>
2025-03-27 16:03:34 +0200

    aalll

--- smb/src/App.jsx
+++ smb/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar  from './components/navbar'
-import './App.css'
+import './css/App.css'
 
 export default function App() {
  
--- smb/src/components/navbar.jsx
+++ smb/src/components/navbar.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import '../css/navbar.css'
 
-export function NavBar() {
+export default function NavBar() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <p>Starting out, count is: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+    <div className='nav__container'>
+<div className='logos'>
+<img src={reactLogo} />
+<h1> BusinessOS</h1>
+
+</div>
+
+      <ul>
+<li>Register</li>
+<li>Login</li>
+<li>Pricing</li>
+
+
+      </ul>
     </div>
   );
 }
